0591e63422a1427dbc925524af8bd0fcae5bfdc2
lcynarski <l.cynarski@gmail.com>
2017-06-09 23:01:32 +0200

    [HM] added google login button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Angular2SocialLoginModule } from 'angular2-social-login';
 
 import { AppComponent }  from './app.component';
 import { routing }        from './app.routing';
@@ -19,12 +20,19 @@ import { MedcomComponent } from './medcom/index';
 import { PatientsListComponent, PatientsListItemComponent } from './patientsList/index';
 import { PatientDetailsComponent } from './patientDetails/index';
 
+const socialProviders = {
+    google: {
+        clientId: '943362751958-kcdagrreknm3e3h26qrasdf3fpupildi.apps.googleusercontent.com'
+    }
+};
+
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule,
-        routing
+        routing,
+        Angular2SocialLoginModule
     ],
     declarations: [
         AppComponent,
@@ -51,6 +59,8 @@ import { PatientDetailsComponent } from './patientDetails/index';
 })
 
 export class AppModule { }
+
+Angular2SocialLoginModule.loadProvidersScripts(socialProviders);
 
 // @NgModule({
 //     imports: [
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-
+import { AuthService } from 'angular2-social-login';
 import { AlertService, AuthenticationService } from '../_services/index';
 
 @Component({
@@ -13,12 +13,28 @@ export class LoginComponent implements OnInit {
     loading = false;
     error = '';
     returnUrl: string;
+    public user = {};
+    sub: any = {};
 
     constructor(
         private router: Router,
         private route: ActivatedRoute,
         private authenticationService: AuthenticationService,
-        private alertService: AlertService) { }
+        private alertService: AlertService,
+        public _auth: AuthService) { }
+
+    signIn(provider = ""){
+        this.sub = this._auth.login(provider).subscribe(
+            (data) => {
+                console.log(data);this.user=data;}
+        );
+    }
+
+    logout(){
+        this._auth.logout().subscribe(
+            (data)=>{console.log(data);this.user=null;}
+        );
+    }
 
     ngOnInit() {
         // reset login status
@@ -31,10 +47,10 @@ export class LoginComponent implements OnInit {
         this.loading = true;
         this.authenticationService.login(this.model.email, this.model.password)
             .subscribe(
-                data => {
+                (data) => {
                     this.router.navigate([this.returnUrl]);
                 },
-                error => {
+                (error) => {
                     this.alertService.error(error._body);
                     this.loading = false;
                 });
