dd818c33fab7fba2d3a975bf96ec3a0894e68888
lcynarski <l.cynarski@gmail.com>
2017-10-25 22:51:14 +0200

    HM - fixed problem with edit patient in patientDetails component, also deleted old button which is replaced with new one

--- src/app/pages/patientDetails/patientDetails.component.ts
+++ src/app/pages/patientDetails/patientDetails.component.ts
@@ -5,6 +5,9 @@ import { Patient } from '../../_models/patient';
 import { PatientService } from '../../_services/patient.service';
 import { DynamicFormComponent } from '../../components/dynamic-form/containers/dynamic-form/dynamic-form.component';
 import { FieldConfig } from '../../components/dynamic-form/models/field-config.interface';
+import createPatientConfig from '../../_forms-configs/create-patient.config';
+import moment = require('moment');
+
 
 @Component({
     providers: [PatientService],
@@ -21,92 +24,17 @@ export class PatientDetailsComponent implements OnInit {
     public id: string;
     public lat: number = 51.678418;
     public lng: number = 7.809007;
+    public config = createPatientConfig;
     // public form: DynamicFormComponent;
     private router: Router;
     private sub: any;
     model: any = {};
-
-    config: FieldConfig[] = [
-        {
-            type: 'input',
-            label: 'First Name',
-            name: 'firstName',
-            placeholder: 'First Name'
-        },
-        {
-            type: 'input',
-            label: 'Last Name',
-            name: 'lastName',
-            placeholder: 'Last Name'
-        },
-        {
-            type: 'input',
-            label: 'Date of birth',
-            name: 'birthdate',
-            placeholder: 'Date'
-        },
-        {
-            type: 'input',
-            label: 'PESEL',
-            name: 'pesel',
-            placeholder: 'PESEL'
-        },
-        {
-            type: 'select',
-            label: 'Gender',
-            name: 'gender',
-            options: ['Male', 'Female', 'Other'],
-            placeholder: 'Select an option'
-        },
-        {
-            type: 'input',
-            label: 'Phone Number',
-            name: 'phoneNumber',
-            placeholder: 'Phone Number'
-        },
-        {
-            type: 'input',
-            label: 'Country',
-            name: 'country',
-            placeholder: 'Country'
-        },
-        {
-            type: 'input',
-            label: 'City',
-            name: 'city',
-            placeholder: 'City'
-        },
-        {
-            type: 'input',
-            label: 'Street',
-            name: 'street',
-            placeholder: 'Street'
-        },
-        {
-            type: 'input',
-            label: 'Building',
-            name: 'buildingNumber',
-            placeholder: 'Building number'
-        },
-        {
-            type: 'input',
-            label: 'Flat',
-            name: 'flatNumber',
-            placeholder: 'Flat number'
-        },
-        {
-            label: 'Submit',
-            name: 'submit',
-            type: 'button'
-        }
-    ];
 
     constructor(router: Router,
                 private http: Http,
                 private route: ActivatedRoute,
                 private patientService: PatientService) {
         this.router = router;
-        // this.id = route.params[0];
     }
 
     public ngOnInit() {
@@ -115,22 +43,15 @@ export class PatientDetailsComponent implements OnInit {
             this.loadPatientData();
             this.loadPatientMedicalData();
         });
-        // this.form.setValue('name', 'Todd Motto');
-        // this.patient.account.personalDetails.keys
-        // for ( item in this.patient.account.personalDetails) {
-        // Object.entries(this.patient.account.personalDetails).forEach(([key, value]) => {
-        //     this.form.setValue(key, value);
-        // };
-        // Object.keys( this.patient.account.personalDetails ).forEach( key => {
-        //     this.form.setValue(key, this.patient.account.personalDetails[key]);
-        // });
-        // }
     }
 
     public loadPatientData() {
         this.patientService.getById(this.id)
             .subscribe((patient) => {
                 this.patient = patient;
+                const { birthdate } = patient;
+                const formattedDate = moment(birthdate).format('YYYY-MM-DD');
+                this.patient = { ...this.patient, birthdate: formattedDate };
                 this.http.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${patient.city}`)
                     .map((res) => res.json())
                     .subscribe((response) => {
@@ -177,8 +98,8 @@ export class PatientDetailsComponent implements OnInit {
     }
 
     onDialogShow = (dialogRef) => {
-        Object.keys(this.patient).forEach(key => {
-            (key !== 'id') && this.form && this.form.setValue(key, this.patient[key]);
+        Object.keys(this.patient).forEach((key) => {
+            (key !== 'id') && (key !== 'insuranceNumber') && this.form && this.form.setValue(key, this.patient[key]);
         });
-    };
+    }
 }
